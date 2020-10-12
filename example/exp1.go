76a1c33b627b593204672509c0bfa2c5ba5c7e23
yefu.chen <yefu.chen@zilliz.com>
2020-10-12 18:42:52 +0800

    exp1

---
+++ example/exp1.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+)
+
+func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome!\n")
+}
+
+func hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprint(w, "hello , %s\n", ps.ByName("name"))
+}
+
+func main() {
+	router := httprouter.New()
+	router.GET("/", index)
+	router.GET("hello/:name", hello)
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
